70f78fae3fb1b16fc9ec1f5089c1fc19f53f8940
“aldair2023” <ammc2023@gmail.com>
2018-03-28 10:30:57 -0500

    v.02

--- src/pruebauno01/PruebaUno01.java
+++ src/pruebauno01/PruebaUno01.java
@@ -17,6 +17,7 @@ public class PruebaUno01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        /*
         Scanner lea = new Scanner (System.in);
         
         int n1, n2, s;
@@ -27,6 +28,32 @@ public class PruebaUno01 {
         n2=Integer.parseInt(lea.next());
         s=n1+n2;
         System.out.println("el resultado es: "+s);
+    
+        */
+        
         
     }
-}
+    public static void promedio(){
+         Scanner lea = new Scanner (System.in);
+        double n1, n2, n3, p;
+         
+         System.out.println("ingrese nota 1");
+         n1=Double.parseDouble(lea.next());
+         System.out.println("ingrese nota 2");
+         n2=Double.parseDouble(lea.next());
+         System.out.println("ingrese nota 3");
+         n3=Double.parseDouble(lea.next());
+         p=(n1+n2+n3)/3;
+         System.out.println("el promedio resultane es: "+p);
+         
+    /*if(p < 3){
+                System.out.println("Lo siento Reprobo");
+            }else {
+                System.out.println("Felicidades!!! Aprobo");
+            }       
+*/
+    
+    }
+    
+}    
+
